5644929bb32561338bed0c2b1c96b70ae1b9365e
Vieira, Alexandre <privado>
2023-04-24 14:38:50 +0100

    oops

--- bitboard.h
+++ bitboard.h
@@ -14,7 +14,7 @@ bboard_t bboard_state(bboard_t bb, uint8_t index);
 bboard_t bboard_state_2(bboard_t bb, uint8_t index);
 bboard_t bboard_set(bboard_t bb, uint8_t index, bboard_t bb_2);
 bboard_t bboard_set_2(bboard_t bb, uint8_t index, bboard_t bb_2);
-bboard_t bboard_flip_vertical(bboard_t bb){
+bboard_t bboard_flip_vertical(bboard_t bb);
 uint8_t bboard_rank(uint8_t index);
 uint8_t bboard_file(uint8_t index);
 uint8_t bboard_index(uint8_t rank, uint8_t file);
